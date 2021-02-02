255c35f74ead008ecee0dce086dc713036ddd3f3
N3r0m5ns3r <ziadh6@hotmail.com>
2021-02-02 21:34:20 +0100

    ex34

--- ex34/darray_tests.c
+++ ex34/darray_tests.c
@@ -7,5 +7,29 @@ static int *val2 = NULL;
 
 char *test_create()
 {
-  array = DArray)
+  array = DArray(sizeof(int), 100);
+  mu_assert(array != NULL, "DArray create failed.");
+  mu_assert(array->contents != NULL, "contents are wrong in darray");
+  mu_assert(array->end == 0, "end isn't at the right spot");
+  mu_assert(array->element_size == sizeof(int), "element size is wrong");
+  mu_assert(array->max == 100, "wrong max length on initial size");
+
+  return NULL;
 }
+
+char *test_destroy()
+{
+  DArray_destroy(array);
+
+  return NULL;
+}
+
+char *test_set()
+{
+  DArray_set(array, 0, val1);
+  DArray_set(array, 1, val2);
+
+  return NULL;
+}
+
+
