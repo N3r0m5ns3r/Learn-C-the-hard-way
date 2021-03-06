aa222babe2b83987401821c801870b0cf6f43e29
N3r0m5ns3r <ziadh6@hotmail.com>
2021-03-06 05:04:01 +0100

    ex35

--- ex35/radixmap/ex35.c
+++ ex35/radixmap/ex35.c
@@ -21,5 +21,40 @@ typedef struct Variant Variant;
 
 void Variant_print(Variant * var)
 {
-  switch (var->)
+  switch (var->type ) {
+    case TYPE_INT:
+      printf("INT: %d\n", var->data.as_integer);
+      break;
+    case TYPE_FLOAT:
+      printf("FLOAT: %f\n", var->data.as_float);
+      break;
+    case TYPE_STRING:
+      printf("STRING %s\n", var->data.as_string);
+      break;
+    default:
+      printf("Unknown type: %d", var->type);
+  }
 }
+
+int main(int argc, char *argv[])
+{
+  Variant a_int = {.type = TYPE_INT, .data.as_integer = 100};
+  Variant a_float = {.type = TYPE_FLOAT, .data.as_float = 100.36};
+  Variant a_string = {.type = TYPE_STRING, .data.as_string = " YO YO "};
+
+  Variant_print(&a_int);
+  Variant_print(&a_float);
+  Variant_print(&a_string);
+
+  //accessing the data
+  a_int.data.as_integer = 200;
+  a_float.data.as_float = 2.345;
+  a_string.data.a_string = "Hi";
+
+  Variant_print(&a_int);
+  Variant_print(&a_float);
+  Variant_print(&a_string);
+
+  return 0;
+}
+
