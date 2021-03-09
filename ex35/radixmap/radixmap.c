32703266245fa82e64c898b9bb47e96c8f766055
N3r0m5ns3r <ziadh6@hotmail.com>
2021-03-09 08:35:02 +0100

    ex35

--- ex35/radixmap/radixmap.c
+++ ex35/radixmap/radixmap.c
@@ -10,5 +10,55 @@
 
 RadixMap *RadixMap_create(size_t max)
 {
+  RadixMap *map = calloc(sizeof(RadixMap), 1);
+  check_mem(map);
 
+  map->contents = calloc(sizeof(RMElement), max + 1);
+  check_mem(map->temp);
+
+  map->max = max;
+  map->end = 0;
+  
+  return map;
+error:
+  return NULL;
+}
+
+void RadixMap_destroy(RadixMap * map)
+{
+  if (map) {
+    free(map->contents);
+    free(map->temp);
+    free(map);
+  }
+}
+
+#define ByteOf(x, y) (((uint8_t *)x)[(y)])
+
+static inline void radix_sort(short offset, uint64_t max, uint64_t * source, uint64_t * dest)
+{
+  uint64_t count[256] = {0};
+  uint64_t *cp = NULL;
+  uint64_t *sp = NULL;
+  uint64_t *end = NULL;
+  uint64_t s = 0;
+  uint64_t c = 0;
+
+  //count occurences of every byte value
+  
+  for (sp = source, end = source + max; sp < end; sp++) {
+    count[ByteOf(sp, offset)]++;
+
+  }
+  //transform count into index by summing
+  //elements and storing into some array
+  for (s = 0, cp = count, end = count + 256; cp < end; cp++){
+    
+    c = *cp;
+    *cp = s;
+    s += c;
+  }
+
+  //fill dest with the right values in the right place
+  for (sp = sou)
 }
