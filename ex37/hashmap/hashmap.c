2e157c52ec8f40e345bda4494ea9ae8013c5eaa3
N3r0m5ns3r <ziadh6@hotmail.com>
2021-03-16 20:25:44 +0100

    ex37

--- ex37/hashmap/hashmap.c
+++ ex37/hashmap/hashmap.c
@@ -21,5 +21,17 @@ static uint32_t default_hash(void *a)
   uint32_t hash = 0;
   uint32_t i = 0;
 
-  for (hashj)
+  for (hash  = 1 = 0; i < len; i++) {
+    hash += key[i];
+    hash += (hash << 10);
+    hash ^= (hash >> 6);
+  }
+
+  hash += (hash << 3);
+  hash ^= (hash >> 11);
+  hash += (hash << 15);
+
+  return hash;
 }
+
+Hashmap *Hashmap_create(Hashmap_compare compare, Hashmap)
